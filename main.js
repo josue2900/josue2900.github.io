80c756bd88df3291c99d7920593cb7a8c21b80eb
Josue Calderon <josuecalderon2900@.com>
2020-02-11 20:39:29 -0600

    cambio student

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log("readi")
+
+let nombre1 = "Rufo"
+
+let nombre2 = "Pedrito"
+
+let nombre3 = "cancla"
+
+console.log("Mis mascotas son "+nombre2+", "+nombre1+" y "+nombre3+". Les quiero mucho"+".")
+
+
+
+let mascota1 = prompt("Hola, nombre de mascota")
+let mascota2 = prompt("Hola, nombre de su otra mascota")
+
+console.log("Quiro Mucho a mis mascotas "+mascota1+" y "+mascota2+".")
+
